6c7348efafea2b0ac6e7f5fcb5835658aaaf0030
James Mosedale <jmdale@protonmail.com>
2021-04-03 01:07:42 -0500

    Add Token class and overload operator<<

--- parser/token.cpp
+++ parser/token.cpp
@@ -1,1 +1,67 @@
 #include "token.h"
+
+#include <iostream>
+
+std::ostream& operator<<(std::ostream &out, const Token &t)
+{
+    out << "TOKEN: TYPE -> ";
+    switch (t.m_type) {
+        case TokenType::INTEGER_L:
+            out << "INTEGER_L";
+            break;
+        case TokenType::STRING_L:
+            out << "STRING_L";
+            break;
+        case TokenType::FLOAT_L:
+            out << "FLOAT_L";
+            break;
+        case TokenType::SEMICOLON:
+            out << "SEMICOLON";
+            break;
+        case TokenType::RBRACE:
+            out << "RBRACE";
+            break;
+        case TokenType::LBRACE:
+            out << "LBRACE";
+            break;
+        case TokenType::RPAREN:
+            out << "RPAREN";
+            break;
+        case TokenType::LPAREN:
+            out << "LPAREN";
+            break;
+        case TokenType::PLUS:
+            out << "PLUS";
+            break;
+        case TokenType::MINUS:
+            out << "MINUS";
+            break;
+        case TokenType::STAR:
+            out << "STAR";
+            break;
+        case TokenType::SLASH:
+            out << "SLASH";
+            break;
+        case TokenType::EQ:
+            out << "EQ";
+            break;
+        case TokenType::IF:
+            out << "IF";
+            break;
+        case TokenType::INT:
+            out << "INT";
+            break;
+        case TokenType::FLOAT:
+            out << "FLOAT";
+            break;
+        case TokenType::VAR:
+            out << "VAR";
+            break;
+        default:
+            out << "No match";
+            break;
+    }
+    out << " | LEXEME -> " << t.m_lexeme;
+
+    return out;
+}
--- parser/token.h
+++ parser/token.h
@@ -1,4 +1,48 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <string_view>
+
+enum class TokenType {
+    /* Literals */
+    INTEGER_L,
+    STRING_L,
+    FLOAT_L,
+
+    /* Punct */
+    SEMICOLON,
+    RBRACE,
+    LBRACE,
+    RPAREN,
+    LPAREN,
+
+    /* Operators */
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    EQ,
+
+    /* Keywords */
+    IF,
+    INT,
+    FLOAT,
+    VAR,
+    // more to come!
+};
+
+class Token {
+private:
+    TokenType m_type;
+    std::string_view m_lexeme;
+
+public:
+    Token(TokenType type, std::string_view lexeme)
+        : m_type{type}, m_lexeme{lexeme}
+    {
+    }
+
+    friend std::ostream& operator<<(std::ostream &out, const Token &t);
+};
+
 #endif
